190756fd325fa18f6947ff3b9da5eaa5910f0f00
Simon Elvery <simon@elvery.net>
2018-06-08 14:06:06 +1000

    Add a babel transpilation for Odyssey code and a couple of polyfills for IE

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
+require('./polyfills');
+
 const Anchors = require('../node_modules/odyssey/src/app/utils/anchors');
 
-function init(){
-  Anchors.getSections([
-    'remove'
-  ]).forEach(section => {
+function init() {
+  Anchors.getSections(['remove']).forEach(section => {
     section.betweenNodes.forEach(node => node.parentNode.removeChild(node));
   });
 }
